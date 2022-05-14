acdb1613a69c83f8759fe46c1377e2b348a975f1
Sarah Barnett <sarah.c.barnett3@gmail.com>
2022-05-14 09:51:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let callBackend = (e) => {
+    fetch('http://localhost:3001/').then(res => res.json()).then(data => {
+      console.log(data)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +25,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <button onClick={callBackend}>
+           Calling Backend
+        </button>
+      </div> 
     </div>
   );
 }
